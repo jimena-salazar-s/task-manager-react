c0a33b8631f95d68d317d61f4f1b78f9b4f16d7a
Jimena Salazar Soto <>
2026-06-29 23:09:40 -0400

    Add initial "GET /tasks" route.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,28 @@
-console.log("Backend is running...");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+type Task = {
+    id: number;
+    text: string;
+    completed: boolean;
+};
+
+const tasks: Task[] = [
+    { id: 1, text: "Estudiar Node.js", completed: false },
+    { id: 2, text: "Crear servidor Express", completed: true },
+    { id: 3, text: "Probar rutas del backend", completed: false }
+];
+
+app.get("/", (req: any, res: any) => {
+    res.send("Backend is working!");
+});
+
+app.get("/tasks", (req: any, res: any) => {
+    res.json(tasks);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
