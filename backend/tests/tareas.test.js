ebe9e81eae24c89fd57930ce21303011bbca82fd
Jimena Salazar Soto <>
2026-09-11 21:52:51 -0400

    ci: romper un unit test a proposito

--- backend/tests/tareas.test.js
+++ backend/tests/tareas.test.js
@@ -26,7 +26,7 @@ describe('API de tareas', () => {
             .set('Authorization', `Bearer ${token}`);
 
         expect(res.status).toBe(200);
-        expect(Array.isArray(res.body)).toBe(true);
+        expect(Array.isArray(res.body)).toBe(false);
     });
 
     it('crea una tarea nueva', async () => {
